d22c54ad23d8985d8df27684a90e2d472a4fdbaa
jellyeast <y135768@gmail.com>
2021-05-18 21:25:14 +0800

    v3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import TodoApp from './TodoApp.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
